a8fb9f4b40590f5db1319f1175110d0ab1559859
vickytaj6459 <vickytaj6459@gmail.com>
2024-08-13 23:28:23 +0545

    sidebar updated

--- src/components/admin-panel/menu.tsx
+++ src/components/admin-panel/menu.tsx
@@ -28,9 +28,10 @@ export function Menu({ isOpen }: MenuProps) {
     <ScrollArea className="[&>div>div[style]]:!block">
       <nav className="mt-8 h-full w-full">
         <ul className="flex flex-col min-h-[calc(100vh-48px-36px-16px-32px)] lg:min-h-[calc(100vh-32px-40px-32px)] items-start space-y-1 px-2">
+          <li className="w-full"></li>
           {menuList.map(({ groupLabel, menus }, index) => (
-            <li className={cn("w-full", groupLabel ? "pt-5" : "")} key={index}>
-              {(isOpen && groupLabel) || isOpen === undefined ? (
+            <li className={cn("w-full", groupLabel ? "pt-0" : "")} key={index}>
+              {/* {(isOpen && groupLabel) || isOpen === undefined ? (
                 <p className="text-sm font-medium text-muted-foreground px-4 pb-2 max-w-[248px] truncate">
                   {groupLabel}
                 </p>
@@ -49,7 +50,7 @@ export function Menu({ isOpen }: MenuProps) {
                 </TooltipProvider>
               ) : (
                 <p className="pb-2"></p>
-              )}
+              )} */}
               {menus.map(
                 ({ href, label, icon: Icon, active, submenus }, index) =>
                   submenus.length === 0 ? (
