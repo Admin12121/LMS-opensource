a21642dba515d84820e32ffc0db409eec93ffb58
vickytaj6459 <vickytaj6459@gmail.com>
2024-08-15 22:12:58 +0545

    layout updated

--- src/app/(user)/(protected)/(dashboard)/layout.tsx
+++ src/app/(user)/(protected)/(dashboard)/layout.tsx
@@ -1,9 +1,24 @@
 import AdminPanelLayout from "@/components/admin-panel/admin-panel-layout";
-
+import { AdminPanel1 } from "@/components/admin-panel/admin-panel1";
+import { cookies } from "next/headers"
 export default function DemoLayout({
   children
 }: {
   children: React.ReactNode;
 }) {
-  return <AdminPanelLayout>{children}</AdminPanelLayout>;
+  const layout = cookies().get("react-resizable-panels:layout:mail")
+  const collapsed = cookies().get("react-resizable-panels:collapsed")
+
+  const defaultLayout = layout ? JSON.parse(layout.value) : undefined
+  const defaultCollapsed = collapsed ? JSON.parse(collapsed.value) : undefined
+  // return <AdminPanelLayout>{children}</AdminPanelLayout>;
+  return (
+    <AdminPanel1
+      defaultLayout={defaultLayout}
+      defaultCollapsed={defaultCollapsed}
+      navCollapsedSize={4}
+    >
+      {children}
+    </AdminPanel1>
+  );
 }
--- src/components/admin-panel/user.tsx
+++ src/components/admin-panel/user.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import {
@@ -9,28 +10,41 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { cn } from "@/lib/utils";
-
 import { LogOut, UserRound, Settings } from "lucide-react";
-interface AccountSwitcherProps {
-    isCollapsed: boolean;
-  }
+import { handleSignOut } from "@/actions/logoutaction";
 
-export function UserNav({
-    isCollapsed,
-  }: AccountSwitcherProps) {
+interface AccountSwitcherProps {
+  isCollapsed: boolean;
+}
+
+export function UserNav({ isCollapsed }: AccountSwitcherProps) {
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button
           variant="secondary"
-          className={cn("relative h-11 w-full p-1 rounded-md flex items-center justify-start", isCollapsed && "h-9 w-9 p-0 justify-center")}
+          className={cn(
+            "relative h-11 w-full p-1 rounded-md flex items-center justify-start",
+            isCollapsed && "h-9 w-9 p-0 justify-center"
+          )}
         >
           <div className="flex items-center space-x-4">
-            <Avatar className={cn("h-9 w-9 rounded-md bg-blue-500", isCollapsed && "h-8 w-8 p-0")}>
+            <Avatar
+              className={cn(
+                "h-9 w-9 rounded-md bg-blue-500",
+                isCollapsed && "h-8 w-8 p-0"
+              )}
+            >
               <AvatarImage src="/avatars/01.png" />
               <AvatarFallback className="bg-transparent">OM</AvatarFallback>
             </Avatar>
-            <div className={cn("flex items-start justify-start flex-col", isCollapsed && "hidden")}>
+            <div
+              className={cn(
+                "flex items-start justify-start flex-col",
+                isCollapsed && "hidden"
+              )}
+            >
               <p className="text-sm font-medium text-muted-foreground leading-none">
                 Sofia Davis
               </p>
@@ -39,23 +53,30 @@ export function UserNav({
           </div>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className={cn("w-56 dark:bg-muted border-0 outline-0", isCollapsed && "left-4 relative")} align="end" forceMount>
+      <DropdownMenuContent
+        className={cn(
+          "w-56 dark:bg-muted border-0 outline-0",
+          isCollapsed && "left-4 relative"
+        )}
+        align="end"
+        forceMount
+      >
         <DropdownMenuGroup>
-          <DropdownMenuItem className="gap-2 hover:bg-neutral-900">
+          <DropdownMenuItem className="gap-2 hover:dark:!bg-neutral-900">
             <UserRound className="w-4 h-4" />
             Profile
             <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
           </DropdownMenuItem>
-          <DropdownMenuItem className="gap-2 hover:bg-neutral-900">
+          <DropdownMenuItem className="gap-2 hover:dark:!bg-neutral-900">
             <Settings className="w-4 h-4" />
             Settings
             <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
           </DropdownMenuItem>
-        <DropdownMenuItem className="gap-2 hover:bg-red-600">
-          <LogOut className="w-4 h-4" />
-          Log out
-          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
-        </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => handleSignOut()} className="gap-2 hover:!bg-destructive/15 hover:!text-red-500 ">
+              <LogOut className="w-4 h-4" />
+              Log out
+              <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+          </DropdownMenuItem>
         </DropdownMenuGroup>
       </DropdownMenuContent>
     </DropdownMenu>
