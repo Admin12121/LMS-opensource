8547cb63a76be6c37604ef84f30ba20edd4eee75
vickytaj6459 <vickytaj6459@gmail.com>
2024-08-29 19:17:19 +0545

    user view updated

--- src/components/browser/courseCard.tsx
+++ src/components/browser/courseCard.tsx
@@ -1,20 +1,88 @@
-"use client"
-
 import Link from "next/link";
-
+import Image from "next/image";
+import { PiBookOpenTextLight } from "react-icons/pi";
 interface CourseProps {
-    title: string;
-    imageUrl: string | null;
-    slug: string;
-    price?: number | null;
-    progress: any | null;
+  title: string;
+  imageUrl: string | null;
+  slug: string;
+  price?: number | null;
+  progress: any | null;
+  category: string;
+  chapter_length: number;
 }
 
-const CourseCard = ({title,imageUrl,price,slug,progress}:CourseProps) => {
-    return (
-        <Link href={`/browser/${slug}`}>
-        </Link>
-    )
-}
+const CourseCard = ({
+  title,
+  imageUrl,
+  price,
+  slug,
+  progress,
+  chapter_length,
+  category,
+}: CourseProps) => {
+  return (
+    <Link href={`/browser/${slug}`}>
+      {/* <div className="group hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full">
+                <div className="relative w-full aspect-video rounded-md overflow-hidden">
+                    <Image
+                     fill
+                     className="object-cover"
+                     alt={title}
+                     src={imageUrl!}
+                    />
+                </div>
+                <div className="flex flex-col pt-2">
+                    <div className="text-lg md:text-base font-medium group-hover:text-sky-700 transition line-clamp-2">
+                        {title}
+                    </div>
+                    <p className="text-xs text-muted-foreground">{category}</p>
+                    <div className="flex items-center gap-x-2 text-sm md:text-xs">
+                        <div className="flex items-center gap-x-1 text-slate-500">
+                            <PiBookOpenTextLight/>
+                            <span>{chapter_length} {chapter_length === 1 ? "Chapter" : "Chapters"}</span>
+                        </div>
+                    </div>
+                    {progress !==null ? (
+                        <div>TODO</div>
+                    ):(<p className="text-md md:text-sm font-medium text-slate-600">{price}</p>)}
+                </div>
+            </div> */}
+      <div className="relative flex w-80 flex-col rounded-xl light:bg-white dark:bg-neutral-900 bg-clip-border light:text-gray-700 shadow-md">
+        <div className="relative mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-blue-500 to-blue-600">
+            <Image
+            src={imageUrl ?  imageUrl:""}
+            alt={title}
+            width={100}
+            height={100}
+            quality={100}
+            priority 
+            className="w-full h-full object-cover"
+            ></Image>
+        </div>
+        <div className="p-6">
+          <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+            {title}
+          </h5>
+          <p className="text-xs text-muted-foreground">{category}</p>
+          <div className="flex items-center gap-x-2 text-sm md:text-xs">
+            <div className="flex items-center gap-x-1 text-slate-500">
+              <span className="border-1 rounded-full p-2"><PiBookOpenTextLight /></span>
+              <span>
+                {chapter_length} {chapter_length === 1 ? "Chapter" : "Chapters"}
+              </span>
+            </div>
+          </div>
+          {/* {progress !== null ? (
+            <div>TODO</div>
+          ) : (
+            <p className="text-md md:text-sm font-medium text-slate-600">
+              {price}
+            </p>
+          )} */}
+        </div>
+      </div>
+    </Link>
+  );
+};
 
 export default CourseCard;
--- src/components/browser/courselist.tsx
+++ src/components/browser/courselist.tsx
@@ -1,15 +1,22 @@
 import React, {useState, useEffect} from 'react'
 import CourseCard from "@/components/browser/courseCard"
 import { SpinnerLoader as Spinner} from "@/components/ui/spinner";
+interface CategoryProps {
+    name: string;
+    categoryslug: string;
+}
 interface Course {
     id: number;
     title: string;
+    chapter_length : number;
     image: string | null;
     courseslug: string;
     price: number | null;
     isPublished: boolean;
     user_progress: any | null;
+    category:CategoryProps;
 }
+
 
 interface CourseListDataProps {
     count: number;
@@ -28,6 +35,7 @@ interface CourselistProps{
 const Courselist = ({setSearch,setPage, data, isLoading}:CourselistProps) => {
     const [course, setCourse] = useState<Course[] | null>(null);
 
+    console.log(course)
     useEffect(() => {
       if (data) {
         setCourse(data.results)
@@ -37,20 +45,24 @@ const Courselist = ({setSearch,setPage, data, isLoading}:CourselistProps) => {
     <>
     {isLoading ? <span className="w-full h-full flex items-center justify-center"><Spinner/></span> :
      (<>
-        {data.results ? <span className="w-full h-full flex items-center justify-center">No courses found</span> : 
+        {!data.results ? <span className="w-full h-full flex items-center justify-center">No courses found</span> : 
         (<>
+        <div className='mt-14'>
            <div className='grid sm:grid-cols-2 md:grid-cols-2 lg: grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4'>
                 {course && course.map((item)=>(
                     <CourseCard
-                     key={item.id}
-                     title={item.title}
-                     imageUrl={item.image}
-                     price={item.price}
-                     slug={item.courseslug}
-                     progress={item.user_progress}
+                    key={item.id}
+                    title={item.title}
+                    imageUrl={item.image}
+                    price={item.price}
+                    slug={item.courseslug}
+                    progress={item.user_progress}
+                    category={item.category.name}
+                    chapter_length={item.chapter_length}
                     />
                 ))}
            </div>
+        </div>
          </>)}
      </>)
      }
